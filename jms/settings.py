769c535d9831a780d1445bf0abb3a8f086ceabf1
wania8383 <nimragill15@gmail.com>
2021-07-01 11:20:34 +0500

    tweaked about title

--- jms/settings.py
+++ jms/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 
 STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
